6c6b3d4b2eb295873ef9c01fb2c3fa9ca18c1919
Lucas Eduardo Rodrigues Barbosa <lucas.barbosa@companhiadasletras.com.br>
2019-10-04 10:30:27 -0300

    teste

--- Cia.Teste/Program.cs
+++ Cia.Teste/Program.cs
@@ -17,6 +17,8 @@ namespace Cia.Teste
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.Write("Lucas");
         }
     }
 }
